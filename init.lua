58cb335b4bc9bcc417d7aceaaad91365c02b31ce
olliy <55839810+olliy1or@users.noreply.github.com>
2021-03-11 22:32:22 +0500

    addz armor, i know very cool

--- init.lua
+++ init.lua
@@ -96,6 +96,57 @@ minetest.register_tool("topaz_items:sword_topaz", {
 	groups = {sword = 1}
 })
 
+-----------
+-- Armor --
+-----------
+
+if minetest.get_modpath("3d_armor") then
+	armor:register_armor("topaz_items:helmet", {
+		description = "Emerald Helmet",
+		inventory_image = "topaz_items_inv_helmet_topaz.png",
+		groups = {armor_head=1, armor_heal=3, armor_use=100, armor_fire=2},
+		armor_groups = {fleshy=5},
+		damage_groups = {cracky=2, snappy=1, level=3},
+		wear = 0,
+	})
+
+	armor:register_armor("topaz_items:chestplate", {
+		description = "Emerald Chestplate",
+		inventory_image = "topaz_items_inv_chestplate_topaz.png",
+		groups = {armor_torso=1, armor_heal=3, armor_use=100, armor_fire=2},
+		armor_groups = {fleshy=10},
+		damage_groups = {cracky=2, snappy=1, level=3},
+		wear = 0,
+	})
+
+	armor:register_armor("topaz_items:leggings", {
+		description = "emerald Leggings",
+		inventory_image = "topaz_items_inv_leggings_topaz.png",
+		groups = {armor_legs=1, armor_heal=3, armor_use=100, armor_fire=2},
+		armor_groups = {fleshy=10},
+		damage_groups = {cracky=2, snappy=1, level=3},
+		wear = 0,
+	})
+
+	armor:register_armor("topaz_items:boots", {
+		description = "emerald Boots",
+		inventory_image = "topaz_items_inv_boots_topaz.png",
+		groups = {armor_feet=1, armor_heal=3, armor_use=100, armor_fire=2, physics_jump=0.5, physics_speed = 1},
+		armor_groups = {fleshy=5},
+		damage_groups = {cracky=2, snappy=1, level=3},
+		wear = 0,
+	})
+
+	armor:register_armor("topaz_items:shield", {
+		description = "emerald Shield",
+		inventory_image = "topaz_items_inv_shield_topaz.png",
+		groups = {armor_shield=1, armor_heal=3, armor_use=100, armor_fire=1},
+		armor_groups = {fleshy=2},
+		damage_groups = {cracky=2, snappy=1, level=3},
+		wear = 0,
+	})
+end
+
 -------------
 -- Crafts --
 -------------
@@ -115,6 +166,10 @@ minetest.register_craft({
 		{"topaz_items:topazblock"},
 	}
 })
+
+--
+-- tools
+--
 
 minetest.register_craft({
 	output = "topaz_items:axe_topaz",
