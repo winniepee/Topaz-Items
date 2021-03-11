a22789c07e4db87f1f3cc4a3387ec53928598232
olliy <55839810+olliy1or@users.noreply.github.com>
2021-03-11 22:44:33 +0500

    oops

--- init.lua
+++ init.lua
@@ -102,7 +102,7 @@ minetest.register_tool("topaz_items:sword_topaz", {
 
 if minetest.get_modpath("3d_armor") then
 	armor:register_armor("topaz_items:helmet", {
-		description = "Emerald Helmet",
+		description = "Topaz Helmet",
 		inventory_image = "topaz_items_inv_helmet_topaz.png",
 		groups = {armor_head=1, armor_heal=3, armor_use=100, armor_fire=2},
 		armor_groups = {fleshy=5},
@@ -111,7 +111,7 @@ if minetest.get_modpath("3d_armor") then
 	})
 
 	armor:register_armor("topaz_items:chestplate", {
-		description = "Emerald Chestplate",
+		description = "Topaz Chestplate",
 		inventory_image = "topaz_items_inv_chestplate_topaz.png",
 		groups = {armor_torso=1, armor_heal=3, armor_use=100, armor_fire=2},
 		armor_groups = {fleshy=10},
@@ -120,7 +120,7 @@ if minetest.get_modpath("3d_armor") then
 	})
 
 	armor:register_armor("topaz_items:leggings", {
-		description = "emerald Leggings",
+		description = "Topaz Leggings",
 		inventory_image = "topaz_items_inv_leggings_topaz.png",
 		groups = {armor_legs=1, armor_heal=3, armor_use=100, armor_fire=2},
 		armor_groups = {fleshy=10},
@@ -129,7 +129,7 @@ if minetest.get_modpath("3d_armor") then
 	})
 
 	armor:register_armor("topaz_items:boots", {
-		description = "emerald Boots",
+		description = "Topaz Boots",
 		inventory_image = "topaz_items_inv_boots_topaz.png",
 		groups = {armor_feet=1, armor_heal=3, armor_use=100, armor_fire=2, physics_jump=0.5, physics_speed = 1},
 		armor_groups = {fleshy=5},
@@ -138,7 +138,7 @@ if minetest.get_modpath("3d_armor") then
 	})
 
 	armor:register_armor("topaz_items:shield", {
-		description = "emerald Shield",
+		description = "Topaz Shield",
 		inventory_image = "topaz_items_inv_shield_topaz.png",
 		groups = {armor_shield=1, armor_heal=3, armor_use=100, armor_fire=1},
 		armor_groups = {fleshy=2},
